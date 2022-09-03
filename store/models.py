56da264d3e49e6379c99d97c8637e002d4984991
Richard <rdanso2003@gmail.com>
2022-09-03 10:40:23 +0000

    edit

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True, blank=True)
 	email = models.CharField(max_length=200)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
